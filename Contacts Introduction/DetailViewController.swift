698ef1879289d78a97cd19c6a79cff68454a2070
Paul <comrade_pasha@ukr.net>
2016-12-05 18:48:49 +0200

    Init commit
    init

---
+++ Contacts Introduction/DetailViewController.swift
@@ -0,0 +1,47 @@
+//
+//  DetailViewController.swift
+//  Contacts Introduction
+//
+//  Created by Davis Allie on 2/01/2016.
+//  Copyright © 2016 tutsplus. All rights reserved.
+//
+
+import UIKit
+import Contacts
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var contactImage: UIImageView!
+    
+    @IBOutlet weak var fullName: UILabel!
+    @IBOutlet weak var address: UILabel!
+    @IBOutlet weak var email: UILabel!
+    
+    var contactItem: CNContact? {
+        didSet {
+            // Update the view.
+            self.configureView()
+        }
+    }
+
+    func configureView() {
+        // Update the user interface for the detail item.
+        if self.contactItem != nil {
+            
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        self.configureView()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
